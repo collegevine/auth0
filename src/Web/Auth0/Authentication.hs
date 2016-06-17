db4b3ee6167118502ce8c256eae6f4c12cff0ed2
Ralph Morton <ralphmorton@gmail.com>
2016-06-17 18:44:05 +0200

    Breaking change: allow configurable scopes for login

--- src/Web/Auth0/Authentication.hs
+++ src/Web/Auth0/Authentication.hs
@@ -13,13 +13,14 @@ import Control.Monad.Except (MonadError)
 import Control.Monad.Reader (MonadReader)
 import Control.Monad.Trans (MonadIO)
 import Data.Aeson
+import Data.List (intersperse)
 import Network.HTTP.Nano
 
 -- |Login with username/password
-login :: (MonadIO m, MonadError e m, MonadReader r m, AsHttpError e, HasHttpCfg r, HasAuth0 r) => String -> String -> String -> String -> m AuthToken
-login clientID connection username password = do
+login :: (MonadIO m, MonadError e m, MonadReader r m, AsHttpError e, HasHttpCfg r, HasAuth0 r) => String -> String -> String -> String -> [String] -> m AuthToken
+login clientID connection username password scopes = do
     url <- (++"oauth/ro") <$> auth0URL
-    let body = object ["client_id" .= clientID, "connection" .= connection, "username" .= username, "password" .= password, "grant_type" .= ("password" :: String), "scope" .= ("openid" :: String)]
+    let body = object ["client_id" .= clientID, "connection" .= connection, "username" .= username, "password" .= password, "grant_type" .= ("password" :: String), "scope" .= concat (intersperse " " scopes)]
     req <- addHeaders [("Content-Type", "application/json")] <$> buildReq POST url (mkJSONData body)
     httpJSON req
 
